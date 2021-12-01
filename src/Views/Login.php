c37be9c7ccbe24b10523364f604d0056816e1c23
gRAKLECLER <guillaume.raklecler@meero.com>
2021-12-01 14:19:35 +0100

    create first two views register/login and add sql file

---
+++ src/Views/Login.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<form method="post">
+    <label for="firstname">Firstname:</label>
+    <input type="text" name="firstname" id="firstname">
+    </br>
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password">
+    <button type="submit" name="login">Login</button>
+    <a href="Register.php">if you're not register yet click here</a>
+</form>
