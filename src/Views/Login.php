5e0f65e9e490998c3d7aaa825e67320a57443b40
gRAKLECLER <guillaume.raklecler@meero.com>
2021-12-03 12:40:41 +0100

    add name input

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -6,12 +6,12 @@
         <h1 class="h3 mb-3 fw-normal">Login</h1>
 
         <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="email" placeholder="name@example.com">
+            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
             <label for="email">Firstname</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="password" class="form-control" id="password" placeholder="Password">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
             <label for="password">Password</label>
         </div>
 
--- src/Views/Register.php
+++ src/Views/Register.php
@@ -5,22 +5,22 @@
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="firstname" placeholder="george">
+                <input type="text" class="form-control" id="firstname" name="firstname" placeholder="george">
                 <label for="firstname">Firstname</label>
             </div>
 
             <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="lastname" placeholder="macgregor">
+                <input type="email" class="form-control" id="lastname" name="lastname" placeholder="macgregor">
                 <label for="lastname">Lastname</label>
             </div>
 
             <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
                 <label for="email">Email address</label>
             </div>
 
             <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="password" placeholder="Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                 <label for="password">Password</label>
             </div>
 
